b081c4e8169896cd75486bee543fb0b877603685
cauealvesb4@gmail.com <cauealvesb4@gmail.com>
2026-02-09 18:48:45 -0300

    feat: Tipando as colunas das tabelas

--- src/@types/knex.d.ts
+++ src/@types/knex.d.ts
@@ -3,7 +3,18 @@ import { Knex } from "knex";
 declare module 'knex/types/tables.js' {
     interface Table {
         Users: {
-
+            Id: string
+            Name: string
+            Height: number
+            Weight: number
+        },
+        Meals: {
+            IdMeals: string,
+            Name: string,
+            Description: string,
+            Date: string,
+            ItsOnTheDiet: boolean,
+            IdUser: string
         }
     }
 }
