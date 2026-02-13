8d2149c37487feee3e77b80c0312dcf9f8461bd2
cauealvesb4@gmail.com <cauealvesb4@gmail.com>
2026-02-13 19:37:38 -0300

    feat: Criando funções para antes e depois dos testes

--- src/test/User/create-user.e2e.test.ts
+++ src/test/User/create-user.e2e.test.ts
@@ -1,9 +1,22 @@
 import request from 'supertest'
-import { describe, it } from 'vitest'
+import { describe, it, beforeAll, beforeEach, afterAll } from 'vitest'
 import { app } from '../../app'
+import { exec } from 'node:child_process'
 
 describe('Create User (E2E)', () => {
 
+    beforeAll( async() => {
+        await app.ready()
+    })
+
+    beforeEach(async () => {
+        exec("npm run knex -- migrate:rollback --all")
+        exec("npm run knex -- migrate:latest")
+    })
+
+    afterAll(async () => {
+        await app.close()
+    })
 
     it('should be able to create a new user', async () => {
         await request(app.server)
